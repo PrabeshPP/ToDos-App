446761c1b297680650b8ae3bd9cae9b77377db9d
Prabesh <prabesh.bista369@gmail.com>
2021-12-25 17:51:44 +0545

    localizations

--- lib/localization.dart
+++ lib/localization.dart
@@ -1,3 +1,23 @@
-class FlutterLocalizations{
-  
+import 'package:flutter/cupertino.dart';
+
+class FlutterLocalizations {
+  static FlutterLocalizations of(BuildContext context) {
+    return Localizations.of(context, FlutterBlocLocalizations);
+  }
+
+  String get appTitle => "ToDo";
+}
+
+class FlutterLocalizationsDelegate
+    extends LocalizationsDelegate<FlutterBlocLocalizations> {
+  @override
+  Future<FlutterBlocLocalizations> load(Locale locale) =>
+      Future(() => FlutterBlocLocalizations());
+
+  @override
+  bool isSupported(Locale locale) =>
+      locale.languageCode.toLowerCase().contains("en");
+
+  @override
+  bool shouldReload(FlutterBlocLocalizationDelegate old) => false;
 }
