ea12c853f92fc6c886ad8f1084babcee8e107304
Prabesh <prabesh.bista369@gmail.com>
2021-12-26 17:37:25 +0545

    event completed

--- lib/bloc/todos_event.dart
+++ lib/bloc/todos_event.dart
@@ -7,9 +7,44 @@ abstract class TodosEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class TodosLoaded extends TodosEvent{}
-class TodosAdded extends TodosEvent{}
-class TodosUpdated extends TodosEvent{}
-class TodosDeleted extends TodosEvent{}
-class ClearCompleted extends TodosEvent{}
-class ToggleAll extends TodosEvent{}
+class TodosLoaded extends TodosEvent {}
+
+class TodosAdded extends TodosEvent {
+  final Todo todo;
+
+  const TodosAdded({required this.todo});
+
+  @override
+  List<Object> get props => [todo];
+
+  @override
+  String toString() => "TodoAdded {todo:$todo}";
+}
+
+class TodosUpdated extends TodosEvent {
+  final Todo todo;
+
+  const TodosUpdated({required this.todo});
+
+  @override
+  List<Object> get props => [todo];
+
+  @override
+  String toString() => "TodoUpdated{todo:$todo}";
+}
+
+class TodosDeleted extends TodosEvent {
+  final Todo todo;
+
+  const TodosDeleted({required this.todo});
+
+  @override
+  List<Object> get props => [todo];
+
+  @override
+  String toString() => "TodoDeleted {todo:$todo}";
+}
+
+class ClearCompleted extends TodosEvent {}
+
+class ToggleAll extends TodosEvent {}
