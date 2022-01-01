f7ab9533948a2bf5936fe1cf40f887a3c321426c
Prabesh <prabesh.bista369@gmail.com>
2022-01-01 10:20:17 +0545

    core

--- packages/TodosRepository/lib/src/enitity/entity.dart
+++ packages/TodosRepository/lib/src/enitity/entity.dart
@@ -1,15 +1,28 @@
 import 'package:equatable/equatable.dart';
 
 class TodoEntity extends Equatable {
- final String id;
+  final String id;
   final bool complete;
   final String note;
   final String task;
 
-  TodoEntity(this.id, this.complete, this.note, this.task);
+  const TodoEntity(
+      {required this.id,
+      required this.complete,
+      required this.note,
+      required this.task});
 
-
+  Map<String, Object> toJson() =>
+      {"complete": complete, "id": id, "note": note, "task": task};
+  static TodoEntity fromJson(Map<String, Object> json) => TodoEntity(
+      task: json['task'] as String,
+      complete: json['complete'] as bool,
+      id: json['id'] as String,
+      note: json['note'] as String);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [id, task, complete, note];
+  @override
+  String toString() =>
+      "TodoEntity{Complete:$complete,task:$task,note:$note,id:$id}";
 }
