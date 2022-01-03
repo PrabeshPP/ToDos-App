4d44be2f0a404dba8e2c968fd7f1e768423fdbb7
Prabesh <prabesh.bista369@gmail.com>
2022-01-03 15:56:11 +0545

    repo

--- packages/TodosRepository/lib/src/todos_repository.dart
+++ packages/TodosRepository/lib/src/todos_repository.dart
@@ -1,11 +1,26 @@
 //this file would help to communicate with local storage
 
+
+
 import 'package:hive/hive.dart';
 
 class TodosRepository {
-  Future<void> getdata() async {
+  Future<void> getData() async {
     var box = await Hive.openBox("MyTask");
     await box.get("MyTask");
     await box.close();
+  }
+
+  Future<void> writeData() async {
+    var box = await Hive.openBox("MyTask");
+    await box.put("hello", "world");
+    await box.close();
+  }
+
+  Future<void> updateData()async{
+
+  }
+  Future<void> deleteData()async{
+
   }
 }
